e20c9d92f12a210c2d8d4dea9de0077dee99f953
BBer <b.bergfeld@student.avans.nl>
2013-02-19 14:58:03 +0100

    Added menu module.
    Used for settings menu (display, navigation, actual setting).

---
+++ IAD/include/menu.h
@@ -0,0 +1,8 @@
+/* 
+ * File:   menu.h
+ * Author: Bas
+ *
+ * Created on 19 februari 2013, 12:45
+ */
+
+extern void menu_open_settings();
---
+++ IAD/source/menu.c
@@ -0,0 +1,13 @@
+#include "display.h"
+#include "keyboard.h"
+#include "menu.h"
+
+// lcd_display_information(char[] *);
+void menu_open_settings()
+{
+    lcd_clear();
+    while(!kb_is_pressed(KEY_ESC))
+    {
+        
+    }
+}
