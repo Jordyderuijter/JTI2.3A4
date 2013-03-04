ffac29279cf067675ea38cecae68c51fd1430aa7
BBer <b.bergfeld@student.avans.nl>
2013-03-04 10:32:47 +0100

    Added hm struct to menu.h

--- IAD/include/menu.h
+++ IAD/include/menu.h
@@ -5,6 +5,13 @@
  * Created on 19 februari 2013, 12:45
  */
 
+// Hours/minutes struct.
+struct hm
+{
+    short hm_hours;             // [0,23]
+    short hm_minutes;           // [0,59]
+};
+
 extern u_short menu_get_current_menu_item(void);
 extern void menu_settings_next_item(void);
 extern void menu_settings_previous_item(void);
